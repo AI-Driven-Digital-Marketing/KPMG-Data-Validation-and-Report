3aaffe930c2685cba42d7e57acf872d0b1ef01d4
Rayden X <102635182+Rayden-Xu@users.noreply.github.com>
2023-02-10 20:07:43 -0500

    Update page_3.py

--- pages/page_3.py
+++ pages/page_3.py
@@ -34,7 +34,7 @@ with tab1:
     col1, col2 = st.columns(2)
     with col1:
 #        image1 = Image.open('src/IMG_5301 2.JPG')
-       st.image('src/IMG_5301 2.JPG', caption='Data World')
+       st.image('src/IMG_5301 2.JPG', "medium")
 
     with col2:
         '''
